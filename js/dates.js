89fda60113550534c98a7faddf4f2a84ef2ab278
er-es <you@example.com>
2021-09-24 18:33:58 -0700

    lesson2 commit

--- js/dates.js
+++ js/dates.js
@@ -1,34 +1,5 @@
 const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
 
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.getElementById("currentdate").textContent = fulldate;
-
+const lastModified = document.getElementById("lastModified");
+lastModified.textContent = `Last Updated: ${document.lastModified}`;

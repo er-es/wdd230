137f716244df26005c7a4a3b2f89c6b3fdd7cf46
er-es <you@example.com>
2021-10-16 22:17:12 -0700

    lesson5 commit

---
+++ lesson5/js/getDates.js
@@ -0,0 +1,17 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+
+
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `${fulldate}`;
+	
+	
+
+
+
+

3349901125d5ff19b574c4d94cbf9c1e91958e86
er-es <you@example.com>
2021-11-11 11:28:59 -0700

    prophets commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,35 +10,33 @@ fetch(requestURL)
     //console.table(jsonObject); for temporary checking for valid response and data parsing
     
     const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) { 
+   
+   // for (let i = 0; i < prophets.length; i++ ) { 
+      
+    prophets.forEach(prophet => {
+        const card = document.createElement('section');
+        const h2 = document.createElement('h2');
+        const birthDate = document.createElement('p');
+        const birthPlace = document.createElement('p');
+        const image = document.createElement('img');
+        const fullname = `${prophet.name} ${prophet.lastname}`;
 
-        const container = document.querySelector('.cards');
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
+        h2.textContent = fullname;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent =`Place of Birth: ${prophet.birthplace}`;
+        image.setAttribute('src', prophet.imageurl);
+        image.setAttribute('alt', `${fullname} - ${prophet.length}`);
 
-        h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
-        
-        let birthDate = document.createElement('p');
-
-        birthDate.textContent = `Date of Birth: ${prophets[i].birthdate}`;
-
-        let birthPlace = document.createElement('p');
-        
-        birthPlace.textContent =`Place of Birth: ${prophets[i].birthplace}`;
-
-        let image = document.createElement('img');
-
-        image.setAttribute('src', prophets[i].imageurl);
-
-      container.appendChild(card)
-      card.appendChild(h2);
-      card.appendChild(birthDate);
-      card.appendChild(birthPlace);
-      card.appendChild(image);
+     
+        card.appendChild(h2);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(image);
 
       document.querySelector('div.cards').appendChild(card);
             
-    }
+    })
   });
+
         
  

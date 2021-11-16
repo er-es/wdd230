6dbe5ee3d2edd1789f7879068323019ab70efbb7
er-es <you@example.com>
2021-11-16 08:54:34 -0700

    lesson 9 commit

--- lesson9/js/hambmenu.js
+++ lesson9/js/hambmenu.js
@@ -1,20 +1,4 @@
 
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const clickMenu = document.querySelector('.clickMenu');
-
-clickMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show () {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-
-function close () {
-    mainMenu.style.top = '-100%';
-}
-
-
-
-//function toggleMenu() { document.getElementsByClassName("navigation")[0].classList.toggle("responsive");}
+function toggleMenu(){
+     document.getElementsByClassName('responsive')[0].classList.toggle('navigation');
+    }
